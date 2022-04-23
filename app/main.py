256011067b924890a664929233f19baebf90bcb7
David Lee <leetdavidu@gmail.com>
2022-04-23 15:04:40 +0800

    updated fastapi schema

--- app/main.py
+++ app/main.py
@@ -1,19 +1,22 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI
 
 from .airtable_ingest import start_ingest
 
 app = FastAPI()
 
-@app.get("/")
+
+@app.get("/", status_code=200)
 def hello():
-    return Response("healthy",
-                    status=200,
-                    mimetype="text/plain")
+    return 'healthy'
+
 
 @app.get("/airtable-ingest")
 def ingest():
     start_ingest()
+    return 'OK'
+
 
 @app.get("/airtable-nuke-and-ingest")
 def nuke_and_ingest():
     ingest()
+    return 'OK'
