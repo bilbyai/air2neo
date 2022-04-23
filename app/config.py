2b6cf16f198a76cde218b07575ae7e714eef7f31
David Lee <leetdavidu@gmail.com>
2022-04-23 17:40:06 +0800

    added logger

---
+++ app/config.py
@@ -0,0 +1,30 @@
+import logging
+from logging.config import dictConfig
+
+# from pydantic import BaseModel
+
+log_config = dict(
+    version=1,
+    disable_existing_loggers=False,
+    formatters={
+        "default": {
+            "()": "uvicorn.logging.DefaultFormatter",
+            "fmt": "%(levelprefix)s %(asctime)s %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    },
+    handlers={
+        "default": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+        },
+    },
+    loggers={
+        "airtable-to-neo4j": {"handlers": ["default"], "level": "DEBUG"},
+    },
+
+)
+
+dictConfig(log_config)
+logger = logging.getLogger('airtable-to-neo4j')
