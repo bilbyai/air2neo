f1132f89223770f94846c9125b34b471eca9db79
David Lee <leetdavidu@gmail.com>
2022-04-25 23:53:19 +0800

    added initial batch create code

--- app/neo4j_functions.py
+++ app/neo4j_functions.py
@@ -1,9 +1,11 @@
-from typing import Sequence
+from typing import Any, Dict, Sequence
 
 from neo4j import Transaction
 
+from .config import airtable_id_col
 
-def create_node(tx: Transaction, label: str, properties: dict) -> None:
+
+def create_node(tx: Transaction, label: str, properties: dict):
     ''' Creates a node in the database.
 
     Args:
@@ -15,5 +17,21 @@ def create_node(tx: Transaction, label: str, properties: dict) -> None:
         f"({label} {{{', '.join(f'{k}: {v}' for k, v in properties.items())}}})",
     )
 
-def create_index_for(tx: Transaction, label: str, indexes: Sequence[str]) -> None:
-    pass
+def create_index_for(tx: Transaction,
+                     label: str,
+                     indexes: Sequence[str]):
+    index_query = ', '.join([f'n.{index}' for index in indexes])
+    cypher_query = f'CREATE INDEX IF NOT EXISTS FOR (n.{label}) ON ({index_query})'
+    tx.run(cypher_query)
+    # TODO not sure if this is the right way to code this
+    return tx.consume()
+
+def batch_create_node(tx: Transaction,
+                      label: str,
+                      nodes_list: Sequence[Dict[str, Any]]):
+    cypher_query = 'UNWIND $nodes_list AS node'
+    cypher_query += f'MERGE (n:{label}) WHERE n.{airtable_id_col} = node.{airtable_id_col}'
+    cypher_query += 'SET n = node'
+    tx.run(cypher_query, nodes_list=nodes_list)
+    # TODO not sure if this is the right way to code this
+    return tx.consume()
