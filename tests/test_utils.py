ff6130f8f2c4b012361183a1f4878b8fcfe4eccf
David Lee <leetdavidu@gmail.com>
2022-09-22 15:37:46 +0800

    added test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,10 +2,10 @@ from air2neo import utils
 
 
 def test_is_airtable_record_id_utils():
-    assert utils.is_airtable_record_id('rec12345678901234') is True
-    assert utils.is_airtable_record_id('recycledblackjack') is True
-    assert utils.is_airtable_record_id('rec!!!!!!!!!!!!!!') is False
-    assert utils.is_airtable_record_id('rec123456789012345') is False
-    assert utils.is_airtable_record_id('rec1234567890123') is False
-    assert utils.is_airtable_record_id('REC12345678901234') is False
+    assert utils.is_airtable_record_id("rec12345678901234") is True
+    assert utils.is_airtable_record_id("recycledblackjack") is True
+    assert utils.is_airtable_record_id("rec!!!!!!!!!!!!!!") is False
+    assert utils.is_airtable_record_id("rec123456789012345") is False
+    assert utils.is_airtable_record_id("rec1234567890123") is False
+    assert utils.is_airtable_record_id("REC12345678901234") is False
     assert utils.is_airtable_record_id(99912345678901234) is False
