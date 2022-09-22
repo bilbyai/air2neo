59d1cb2968f490dc2e27534eb249181223b9ed88
David Lee <leetdavidu@gmail.com>
2022-09-22 15:38:09 +0800

    formatting

--- air2neo/config.py
+++ air2neo/config.py
@@ -1,5 +1,5 @@
 def keep_col_rule_default(column_name: str) -> bool:
-    ''' Checks if a column name should be kept.
+    """Checks if a column name should be kept.
 
     Args:
         column_name (str): The name of the column.
@@ -7,14 +7,14 @@ def keep_col_rule_default(column_name: str) -> bool:
     Returns:
         bool: Returns true if the column should be kept, and false if it should
         be discarded.
-    '''
+    """
     if not isinstance(column_name, str):
         return False
-    return not column_name.startswith('_')
+    return not column_name.startswith("_")
 
 
 def is_edge_rule_default(column_name: str) -> bool:
-    ''' Checks if a column name is an edge column or a node property column.
+    """Checks if a column name is an edge column or a node property column.
 
     Args:
         column_name (str): The name of the column.
@@ -22,14 +22,14 @@ def is_edge_rule_default(column_name: str) -> bool:
     Returns:
         bool: Returns true if the column is an edge column, and false if it is
         a node property column.
-    '''
+    """
     if not isinstance(column_name, str):
         return False
     return column_name.isupper()
 
 
 def is_prop_rule_default(column_name: str) -> bool:
-    ''' Checks if a column name is a node property column.
+    """Checks if a column name is a node property column.
 
     Args:
         column_name (str): The name of the column.
@@ -37,14 +37,14 @@ def is_prop_rule_default(column_name: str) -> bool:
     Returns:
         bool: Returns true if the column is a node property column, and false
         if it is an edge column.
-    '''
+    """
     if not isinstance(column_name, str):
         return False
     return not is_edge_rule_default(column_name)
 
 
 def format_edge_col_name_default(col: str) -> str:
-    ''' Formats an edge column name.
+    """Formats an edge column name.
     Anything after a dunder (double underline) is removed.
 
     Args:
@@ -52,5 +52,5 @@ def format_edge_col_name_default(col: str) -> str:
 
     Returns:
         str: The formatted column name.
-    '''
-    return col.split('__')[0]
+    """
+    return col.split("__")[0]
