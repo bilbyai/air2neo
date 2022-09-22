adfc02245626ae0135a772f12687a3347055cb3b
David Lee <leetdavidu@gmail.com>
2022-09-22 15:37:39 +0800

    added timestamp util

--- air2neo/utils.py
+++ air2neo/utils.py
@@ -1,8 +1,9 @@
+import datetime
 from typing import Any
 
 
 def is_airtable_record_id(record: Any) -> bool:
-    ''' Checks if a single record is an airtable ID.
+    """Checks if a single record is an airtable ID.
     An airtable ID is defined by 3 things:
     1. It is a string.
     2. All characters are alphanumeric.
@@ -15,10 +16,23 @@ def is_airtable_record_id(record: Any) -> bool:
     Returns:
         bool: Returns true if the record is an airtable ID, and false if it is
         not.
-    '''
+    """
     return (
         isinstance(record, str)
         and record.isalnum()
         and len(record) == 17
         and record.startswith("rec")
     )
+
+
+def get_airtable_timestamp_str(dt: datetime.datetime) -> str:
+    """Converts a datetime object to an airtable-friendly timestamp string.
+    Returns a timestamp in the following format: '2022-09-20T06:08:51.601Z'
+
+    Args:
+        dt (datetime.datetime): A datetime object.
+
+    Returns:
+        str: A timestamp string.
+    """
+    return dt.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
